d3a686eb3aaa77365a610a9420f1ff067744e82b
Fatima-Zahra Bendnane <Fatimazahrabendnane@gmail.com>
2025-11-17 11:16:50 +0100

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -1,44 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbendnan <fbendnan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 10:38:17 by fbendnan          #+#    #+#             */
+/*   Updated: 2025/11/17 11:13:17 by fbendnan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char	*extract_line(char **buffer)
+char	*extract_line(char **storage)
 {
     char    *line;
-	char	*newline;
+	char	*newline_p;
 
-	newline = ft_strchr(buffer, '\n');
-	line = ft_strlen(buffer) - ft_strlen(newline) + 1; 
-	line = ft_substr(buffer, 0, ft_strlen(buffer) - ft_strlen(newline));
-	buffer = newline++;
-
+	newline_p = ft_strchr(*storage, '\n');
+	line = ft_substr(*storage, 0, (ft_strlen(*storage) - ft_strlen(newline_p)) + 1);
+	free(*storage);
+	*storage = NULL;
+	*storage = newline_p++;
 	return (line);
 }
 
-char    *read_buffer(int fd, char **buffer)
+void    read_buffer(int fd, char **storage)
 {
-	char	*line;
-	char	buff[BUFFER_SIZE];
+	char	*tmp_line;
+	char	buffer[BUFFER_SIZE];
+	int		read_return;
 
-	while (read(fd, buff, 0))
+	read_return = read(fd, buffer, BUFFER_SIZE);
+	while (read_return && !ft_strchr(*storage, '\n'))
 	{
-		read(fd, buff, BUFFER_SIZE);
-		line = ft_strjoin(buffer, buff);
-		if (ft_strchr(buff, '\n'))
-			return (1);
-		read(fd, buff, BUFFER_SIZE);
+		tmp_line = ft_strjoin(*storage, buffer);
+		*storage = ft_strdup(tmp_line);
+		read_return = read(fd, buffer, BUFFER_SIZE);
 	}
-	return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	static char **buffer;
+	static char *storage;
+	char	*line;
 
 	if (fd == -1)
 		return (NULL);
-	
-	// if (!buffer)
-	// 	buffer = ft_strdup("");
-	if(read_buffer(fd, buffer))
-		return (extract_line(buffer));
-	free(buffer);
+		if(!storage)
+			storage = ft_strdup("");
+	read_buffer(fd, *storage);
+	line = extract_line(*storage);
 }
